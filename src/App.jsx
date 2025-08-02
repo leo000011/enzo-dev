9072b912547567515370e1e22e012653e7baf4af
Leo <leandroano8@gmail.com>
2025-08-02 19:01:55 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import EbookSalesPage from './page/vendaPage/VendaPage1';
 
 function App() {
   return (
-<<<<<<< HEAD
     <>
       <BrowserRouter>
         <Routes>
@@ -14,22 +13,6 @@ function App() {
         </Routes>
       </BrowserRouter>
     </>
-=======
-    <div className='bg-gray-800 h-screen p-5 flex justify-center items-center flex-col gap-3'>
-        <div className='flex gap-2 items-center bg-white p-2 rounded hover:bg-yellow-400'>
-             <img src={imagem} alt="enzo" className=' h-[60px] w-[60px] rounded-full  border-solid border-white border-2' />
-            <h2 className='text-black font-medium'>@_enzo.dev</h2>
-        </div>
-        <div className='flex flex-col gap-2'>
-             
-             <Button text='Instagram' link="https://www.instagram.com/_enzo.dev/" />
-
-             <Button text="TikTok" link="https://www.tiktok.com/@_enzo.dev0?_t=ZM-8xsKbIRvCGE&_r=1" />
-
-        </div>
-      
-    </div>
->>>>>>> ba431ab46cb3211b4ca2956f541500337a47fef1
   )
 }
 
